365f9cbc258af5c41344f522871ee36e8c9384f6
Brian Wylie <bwylie@vectranetworks.com>
2015-09-20 17:41:37 -0600

    bump version

--- chains/__init__.py
+++ chains/__init__.py
@@ -1,5 +1,5 @@
 ''' chains project '''
 __author__ = 'Brian Wylie'
 __author_email__ = 'briford@supercowpowers.com'
-__version__ = '0.1.10'
+__version__ = '0.1.12'
 __url__ = 'https://github.com/SuperCowPowers/chains'
