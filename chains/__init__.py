046b9e280e0611583ce9ca8df2f9cfdfd18b43e8
Brian Wylie <briford.wylie@gmail.com>
2017-03-24 16:08:00 -0600

    bump version for python 3 release

--- chains/__init__.py
+++ chains/__init__.py
@@ -1,5 +1,5 @@
 ''' chains project '''
 __author__ = 'Brian Wylie'
 __author_email__ = 'briford@supercowpowers.com'
-__version__ = '0.1.17'
+__version__ = '0.2.0'
 __url__ = 'https://github.com/SuperCowPowers/chains'
